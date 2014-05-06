9c98f99871f2bbbc4ba43850216f147ea127b1b8
arg0070 <arg0070@alu.ubu.es>
2014-05-06 17:09:49 +0200

    Refactorización:
    Move method 'ubu.gii.dass.refactoring.Rental.getFrequentRenterPoints()'
    to 'ubu.gii.dass.refactoring.Rental._movie'
    - Original project: 'refactoring-fowler-example'
    - Original element:
    'ubu.gii.dass.refactoring.Rental.getFrequentRenterPoints()'
    - Keep original element as delegate to refactored element
    - Moved method: N/A
    - Target element: 'ubu.gii.dass.refactoring.Rental._movie'
    - Method name: 'getFrequentRenterPoints'
    - Parameter name: 'rental'
    
    Move method 'ubu.gii.dass.refactoring.Rental.getFrequentRenterPoints()'
    to 'ubu.gii.dass.refactoring.Rental._movie'
    - Original project: 'refactoring-fowler-example'
    - Original element:
    'ubu.gii.dass.refactoring.Rental.getFrequentRenterPoints()'
    - Keep original element as delegate to refactored element
    - Moved method: N/A
    - Target element: 'ubu.gii.dass.refactoring.Rental._movie'
    - Method name: 'getFrequentRenterPoints'
    - Parameter name: 'rental'

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -74,4 +74,18 @@ public class Movie {
 		}
 		return result;
 	}
+
+	public int getFrequentRenterPoints(Rental rental) {
+		return getFrequentRenterPoints(rental);
+	}
+
+	public int getFrequentRenterPoints(int daysRented) {
+		// add frequent renter points
+		int frequentRenterPoints = 1;;
+		// add bonus for a two day new release rental
+		if ((getPriceCode() == Movie.NEW_RELEASE)
+				&& daysRented > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
