1fb9e51604bdd49790a114936405568ed8a66c00
arg0070 <arg0070@alu.ubu.es>
2014-05-06 16:56:29 +0200

    Modificados los métodos y atributos de la clase Movie.

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -1,11 +1,12 @@
 package ubu.gii.dass.refactoring;
+
 /**
  * Tema Refactorizaciones
  * 
- * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
- * genéricas de java 1.5
+ * Ejemplo de aplicaciï¿½n de refactorizaciones. Actualizado para colecciones
+ * genï¿½ricas de java 1.5
  * 
- * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos Lï¿½pez</A>
  * @version 1.1
  * @see java.io.File
  * 
@@ -17,19 +18,32 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 
 	private String _title;
-	private int _priceCode;
+	private Price _priceCode;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
-		_priceCode = priceCode;
+		setPriceCode(priceCode);
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _priceCode.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+
+		case CHILDRENS:
+			_priceCode = new ChildrenPrice();
+			break;
+		case REGULAR:
+			_priceCode = new RegularPrice();
+			break;
+		case NEW_RELEASE:
+			_priceCode = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("CÃ³digo invÃ¡lido.");
+		}
 	}
 
 	public String getTitle() {
