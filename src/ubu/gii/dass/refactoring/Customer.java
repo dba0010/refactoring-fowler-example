ae9ec1faed313c62356c4fbf182a5615140ba96b
arg0070 <arg0070@alu.ubu.es>
2014-05-06 13:47:30 +0200

    Refactoring Extract method 'private int getTotalFrequentRenterPoints(int frequentRenterPoints, Rental each)' from 'ubu.gii.dass.refactoring.Customer.statement()' to 'ubu.gii.dass.refactoring.Customer' - Original project: 'refactoring-fowler-example' - Method name: 'getTotalFrequentRenterPoints' - Destination type: 'ubu.gii.dass.refactoring.Customer' - Declared visibility: 'private'
    Extract method 'private int getTotalFrequentRenterPoints(int
    frequentRenterPoints, Rental each)' from
    'ubu.gii.dass.refactoring.Customer.statement()' to
    'ubu.gii.dass.refactoring.Customer'
    - Original project: 'refactoring-fowler-example'
    - Method name: 'getTotalFrequentRenterPoints'
    - Destination type: 'ubu.gii.dass.refactoring.Customer'
    - Declared visibility: 'private'
    
    Extract method 'private int getTotalFrequentRenterPoints(int
    frequentRenterPoints, Rental each)' from
    'ubu.gii.dass.refactoring.Customer.statement()' to
    'ubu.gii.dass.refactoring.Customer'
    - Original project: 'refactoring-fowler-example'
    - Method name: 'getTotalFrequentRenterPoints'
    - Destination type: 'ubu.gii.dass.refactoring.Customer'
    - Declared visibility: 'private'

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -1,15 +1,15 @@
 package ubu.gii.dass.refactoring;
 
 /**
-* Tema  Refactorizaciones 
-*
-* Ejemplo de aplicaci�n de refactorizaciones. Actualizado para colecciones gen�ricas de java 1.5
-*
-* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos L�pez</A>
-* @version 1.1
-* @see java.io.File
-*
-*/
+ * Tema  Refactorizaciones 
+ *
+ * Ejemplo de aplicaci�n de refactorizaciones. Actualizado para colecciones gen�ricas de java 1.5
+ *
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos L�pez</A>
+ * @version 1.1
+ * @see java.io.File
+ *
+ */
 import java.util.*;
 
 public class Customer {
@@ -32,15 +32,12 @@ public class Customer {
 
 	public String statement() {
 		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
+		Enumeration<Rental> rentals = _rentals.elements();
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = rentals.nextElement();
+			double thisAmount = 0;
 			thisAmount = each.getCharge();
-			frequentRenterPoints = each.getFrequentRenterPoints(
-					frequentRenterPoints);
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -48,8 +45,19 @@ public class Customer {
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "You earned "
+				+ String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			result = each.getFrequentRenterPoints(result);
+		}
 		return result;
 	}
 }
